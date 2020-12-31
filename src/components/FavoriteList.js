5042f651d5ff92f9d738ca5645b93e9d08e065a2
Sean Cassiere <sean.cassiere@gmail.com>
2021-01-01 01:08:47 +0530

    Read-Write from LocalStorage

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import {
   Box,
   Container,
@@ -15,7 +15,10 @@ import { useSelector, useDispatch } from "react-redux"
 
 import { DeleteIcon } from "@chakra-ui/icons"
 
-import { REMOVE_FROM_FAVORITES } from "../constants/favoritesConstants"
+import {
+  LIST_ALL_FAVORITES,
+  REMOVE_FROM_FAVORITES,
+} from "../constants/favoritesConstants"
 
 const FavoriteList = () => {
   const dispatch = useDispatch()
@@ -26,9 +29,29 @@ const FavoriteList = () => {
     dispatch({ type: REMOVE_FROM_FAVORITES, payload: id })
   }
 
+  const handleSetItem = () => {
+    const data = [
+      {
+        fromCurrency: "USD",
+        fromAmount: "0.00",
+        toCurrency: "LKR",
+        toAmount: "0.00",
+        itemName: "",
+        date: "1/1/2021, 12:59:27 AM",
+      },
+    ]
+    const req = JSON.stringify(data)
+    localStorage.setItem("SC_CURR_FAVS", req)
+  }
+
+  useEffect(() => {
+    dispatch({ type: LIST_ALL_FAVORITES })
+  }, [dispatch])
+
   return (
     <div style={{ marginTop: "4rem", marginBottom: "2rem" }}>
       <Container maxW='4xl' centerContent>
+        <button onClick={handleSetItem}>Click</button>
         <Box padding='4' bg='white' maxW='4xl' w='100%'>
           <Table variant='striped'>
             <TableCaption>Your saved currency conversions</TableCaption>
--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,14 +1,26 @@
 import {
   ADD_TO_FAVORITES,
   REMOVE_FROM_FAVORITES,
+  LIST_ALL_FAVORITES,
 } from "../constants/favoritesConstants"
 
 export const favoritesReducers = (state = [], action) => {
   switch (action.type) {
+    case LIST_ALL_FAVORITES:
+      const localData = localStorage.getItem("SC_CURR_FAVS")
+      if (localData) {
+        return JSON.parse(localData)
+      } else {
+        return state
+      }
     case ADD_TO_FAVORITES:
-      return [action.payload, ...state]
+      const addData = [action.payload, ...state]
+      localStorage.setItem("SC_CURR_FAVS", JSON.stringify(addData))
+      return addData
     case REMOVE_FROM_FAVORITES:
-      return state.filter((item) => item !== action.payload)
+      const remData = state.filter((item) => item !== action.payload)
+      localStorage.setItem("SC_CURR_FAVS", JSON.stringify(remData))
+      return remData
     default:
       return state
   }
