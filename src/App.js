d8134cacf84b6e4ec3d6e926fd561e9ae9f68b94
Sean Cassiere <sean.cassiere@gmail.com>
2020-12-31 18:31:01 +0530

    Added Chakra

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react"
+import "./App.css"
+import { Heading } from "@chakra-ui/react"
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <header className='header'></header>
+      <Heading as='h1' size='2xl'>
+        Hello World
+      </Heading>
+      <Heading as='h2' size='lg'>
+        My exchangerate-api key is {process.env.REACT_APP_API_KEY}
+      </Heading>
     </div>
   )
 }
